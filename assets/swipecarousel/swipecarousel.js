4b7dc6572f00d1201e888f30e5a06366538b3c0d
Tuyen Hong <tuyennh3008@gmail.com>
2023-01-10 15:57:40 +0700

    first commit

---
+++ assets/swipecarousel/swipecarousel.js
@@ -0,0 +1,98 @@
+
+/*var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+*/
+
+var swiper = new Swiper(".mySwiper1", {});
+var swiper = new Swiper(".mySwiper2", {});
+
+var swiper = new Swiper(".mySwiper3", {
+  slidesPerView: "auto",
+  spaceBetween: 30,
+});
+
+var swiper = new Swiper(".mySwiper6", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,  
+});
+
+var swiper = new Swiper(".mySwiper5", {
+  spaceBetween: 10,
+  thumbs: {
+    swiper: swiper
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".mySwiper7", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+
+var swiper = new Swiper(".mySwiper8", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
+  },
+});
+
+
+
+var swiper = new Swiper(".mySwiper9", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
